ab6608478ee40491ead5045bd70407c2c22d9453
Coder-Tech <57731369+Coder-Tech@users.noreply.github.com>
2020-11-12 10:02:40 -0800

    Update main_rpg.py

--- main_rpg.py
+++ main_rpg.py
@@ -74,6 +74,10 @@ async def on_ready():
 async def change_status():
     await client.change_presence(activity=discord.Game(next(status)))
 
+@client.command()
+async def create_profile(ctx, class=None):
+    await ctx.send("smh specify a class")
+    
 #@client.command(aliases=["create"])
 #async def create_profile(ctx):
 #    await ctx.send("tst")
